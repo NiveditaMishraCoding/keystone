84829dcca84480661e8d4fbe0eb321c1a9d39e35
NiveditaMishraCoding <niveditamasteredu@gmail.com>
2023-04-25 00:18:49 -0700

    dockerization of tippers

---
+++ src/main/java/com/developer/keystone/service/ResidentService.java
@@ -0,0 +1,10 @@
+package com.developer.keystone.service;
+
+import com.developer.keystone.domain.Resident;
+import com.developer.keystone.entity.FacilityData;
+
+import java.util.List;
+
+public interface ResidentService {
+    List<Resident> getAllResidents();
+}
